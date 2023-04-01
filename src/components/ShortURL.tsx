3a4c0ae494cbdf0300cdaeab23a092047c9e5c2c
Carlos Arbizu <carlos2345532@gmail.com>
2023-04-01 16:52:41 -0500

    accessbility navbar, added line for card components, started responsiveness design

--- src/components/ShortURL.tsx
+++ src/components/ShortURL.tsx
@@ -25,14 +25,17 @@ export const ShortURL = (props: { shortURL: string, longURL: string}) => {
   return (
     <li className='bg-white w-[calc(100%-3rem)] relative left-6 rounded-sm'> 
       <article className='pb-4 h-shortLinkArticle'>
-     <div className='pl-4 h-shortLinkH1Height flex items-center'> <h1 aria-label='long URL' className='tracking-wide w-[100%] overflow-hidden whitespace-nowrap text-ellipsis' >{props.longURL}</h1> </div>
-    <div className='pl-4 pr-4 border-t border-t-secondarytxt text-secondary tracking-wide h-[calc(100%-3rem)] flex flex-col justify-between pt-[.4375rem]'>
-        <div className='h-[2.25rem] flex items-center'> 
-        <h2 aria-label='short URL' className='text-primary w-[100%] overflow-hidden whitespace-nowrap text-ellipsis'>{props.shortURL}</h2>
-      </div>
-        <button className='self-center max-w-copyBtn w-btnXlg h-btnSm bg-primary disabled:bg-secondary focus:bg-primary-hover hover:bg-primary-hover text-white' onClick={handleCopy} disabled={isCopied} aria-pressed={isCopied} aria-label='Copy the short URL'>{isCopied ? 'Copied' : 'Copy'}</button>
-      </div>
-    </article> 
+        <div className='pl-4 h-shortLinkH1Height flex items-center'> 
+          <h1 aria-label='long URL' className='text-primarytxt tracking-wide w-[100%] overflow-hidden whitespace-nowrap text-ellipsis' >{props.longURL}</h1> 
+          
+        </div>
+        <div className='pl-4 pr-4 border-t border-t-secondarytxt text-secondary tracking-wide h-[calc(100%-3rem)] flex flex-col justify-between pt-[.4375rem]'>
+          <div className='h-[2.25rem] flex items-center'> 
+            <h2 aria-label='short URL' className='text-primary w-[100%] overflow-hidden whitespace-nowrap text-ellipsis'>{props.shortURL}</h2>
+          </div>
+          <button className='self-center max-w-copyBtn w-btnXlg h-btnSm bg-primary disabled:bg-secondary focus:bg-primary-hover hover:bg-primary-hover text-white' onClick={handleCopy} disabled={isCopied} aria-pressed={isCopied} aria-label='Copy the short URL'>{isCopied ? 'Copied' : 'Copy'}</button>
+        </div>
+      </article> 
     </li>
   );
 }
